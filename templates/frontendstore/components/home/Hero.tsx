3f82560e279ea7daba9c104548eb7c8c980463cd
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-05-18 18:29:22 +0200

    Hero

--- templates/frontendstore/components/home/Hero.tsx
+++ templates/frontendstore/components/home/Hero.tsx
@@ -3,16 +3,53 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section>
-      <div>
-        <motion.div animate={{ rotateY: 70 }}>
-          <Image
-            alt="React"
-            src="/techs/ReactLogo.svg"
-            width={800}
-            height={700}
-          />
-        </motion.div>
+    <section className="relative group">
+      <h1 className="text-center absolute top-1/2 right-1/2 z-10 translate-x-1/2 text-fuchsia-600 -translate-y-1/2 text-7xl group-hover:blur-3xl group-hover:opacity-0 transition-all duration-300 ease-in-out">
+        Hot right now
+      </h1>
+      <div className="grid grid-cols-12 justify-items-center grayscale-[50%] group-hover:filter-none transition-all duration-600 ease-in-out">
+        <div className="p-12 bg-sky-900 col-span-8">
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.5, ease: "linear" }}
+          >
+            <Image
+              alt="React"
+              src="/techs/ReactLogo.svg"
+              width={1200}
+              height={1068}
+              className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out"
+            />
+          </motion.div>
+        </div>
+        <div className="col-span-4 w-full h-full flex flex-col justify-around">
+          <div className="p-12 bg-black flex justify-center items-center h-full">
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.3, ease: "linear" }}
+            >
+              <Image
+                src="/techs/next-black.svg"
+                width={414}
+                height={248}
+                className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out"
+              />
+            </motion.div>
+          </div>
+          <div className="p-12 bg-violet-300 flex justify-center items-center h-full">
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.3, ease: "linear" }}
+            >
+              <Image
+                src="/techs/reduxlogo.svg"
+                width={400}
+                height={400}
+                className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out"
+              />
+            </motion.div>
+          </div>
+        </div>
       </div>
     </section>
   );
