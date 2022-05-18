653c0036df7e1031fe930f89496831b05fb024b7
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-05-18 11:14:48 +0200

    Added tailwind and started working on fe store

---
+++ templates/frontendstore/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return <header className="w-11/12 max-w-7xl mx-auto">
+    <div className="border-b-2 border-b-neutral-300 px-8 py-4">
+      <h1>Front-end Store</h1>
+    </div>
+  </header>;
+}
