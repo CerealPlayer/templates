3af82bda07ea84de19eedd90d0c3fe920b2a445b
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-05-20 21:11:46 +0200

    Add product page

---
+++ templates/frontendstore/pages/products/index.tsx
@@ -0,0 +1,17 @@
+import { readFile } from "fs/promises";
+import Products from "../../components/home/Products";
+import type { products } from "../../types/props";
+
+export default function ProducPage({ products }: { products: products }) {
+  return <Products products={products} />;
+}
+
+export async function getStaticProps() {
+  const data = await readFile("./data/products.json");
+  const products = JSON.parse(data.toString());
+  return {
+    props: {
+      products: products,
+    },
+  };
+}
