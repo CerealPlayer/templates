a2f9fc00e07b1fecbd554ed625c4f0f676f35a81
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-05-21 00:03:06 +0200

    Restyled btn

--- templates/frontendstore/components/UI/Button.tsx
+++ templates/frontendstore/components/UI/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ children, dis }: props) {
       disabled={dis}
       className="px-8 group py-2 relative flex justify-center items-center"
     >
-      <div className="absolute top-0 left-0 w-full h-full outline outline-1 outline-neutral-500 scale-x-0 group-hover:scale-x-100 transition-all duration-200 ease-in-out"></div>
+      <div className="absolute top-0 left-0 w-full h-full border-b border-neutral-500 scale-x-0 group-hover:scale-x-100 transition-all duration-200 ease-out"></div>
       {children}
     </button>
   );
