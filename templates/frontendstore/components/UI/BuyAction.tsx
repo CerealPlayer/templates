8e5435aa03e2d3140e17aff9f239c9d8647bea07
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-05-20 19:18:30 +0200

    Style the buy action

--- templates/frontendstore/components/UI/BuyAction.tsx
+++ templates/frontendstore/components/UI/BuyAction.tsx
@@ -1,18 +1,18 @@
 export default function BuyAction() {
   return (
     <>
-      <aside
-        className="mx-auto w-full xl:col-span-3 border-2 rounded-xl p-4 border-slate-400"
-      >
-        <h2>React</h2>
-        <div>
-          <h3>Elige una opción</h3>
-          <select>
-            <option>18.1</option>
-            <option>18.0</option>
-            <option>17.0.2</option>
-            <option>17.0.1</option>
-          </select>
+      <aside className="mx-auto w-full xl:col-span-3 border-2 rounded-xl p-4 border-slate-400">
+        <div className="flex justify-end">
+          <span className="text-3xl text-slate-200 font-bold bg-slate-600 py-1 px-2 rounded-md">18.10 €</span>
+        </div>
+        <div className="my-8">
+          <h3>Includes</h3>
+          <ul className=" text-slate-600 list-disc list-outside pl-4">
+            <li>Huge community</li>
+            <li>Thousands of librarys that will make your job easier</li>
+            <li>Hooks</li>
+            <li>Backed by Facebook</li>
+          </ul>
         </div>
       </aside>
     </>
