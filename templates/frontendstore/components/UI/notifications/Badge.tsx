a0868c800404e6ce567648b3e3962169319c751e
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-05-21 19:22:07 +0200

    Badge animation

--- templates/frontendstore/components/UI/notifications/Badge.tsx
+++ templates/frontendstore/components/UI/notifications/Badge.tsx
@@ -1,29 +1,36 @@
-import { ReactNode, useEffect, useContext } from "react";
+import { ReactNode, useEffect, useContext, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { CartContext } from "../../../context/cart-context";
 
 export default function Badge({ children }: { children: ReactNode }) {
+  const [isEmpty, setIsEmpty] = useState(false);
   const ctx = useContext(CartContext);
   const controls = useAnimation();
+  console.log(ctx);
   useEffect(() => {
-    if (ctx.items.length > 0) {
-      controls.start({
-        y: [0, -10, 5, 0],
-        transition: {
-          type: "spring",
-          duration: 0.5,
-          repeat: 2,
-          repeatDelay: 0.2,
-        },
-      });
+    if (ctx.items.length >= 1) {
+      setIsEmpty(false);
+    } else {
+      setIsEmpty(true);
     }
+    controls.start({
+      y: [0, -10, 5, 0],
+      transition: {
+        type: "spring",
+        duration: 0.3,
+        repeat: 2,
+        repeatDelay: 0.1,
+      },
+    });
   }, [ctx]);
   return (
     <div className="relative">
-      <motion.div
-        animate={controls}
-        className="absolute w-3 h-3 bg-[#8B7B64] rounded-full top-0 right-0"
-      ></motion.div>
+      {!isEmpty && (
+        <motion.div
+          animate={controls}
+          className="absolute w-3 h-3 bg-[#8B7B64] rounded-full top-0 right-0"
+        ></motion.div>
+      )}
       {children}
     </div>
   );
