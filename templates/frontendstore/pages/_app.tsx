eb202bf96396d9f552e424a471e020ce10f0f8b0
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-05-23 12:39:56 +0200

    Added some head tags

--- templates/frontendstore/pages/_app.tsx
+++ templates/frontendstore/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
 import { CartContextProvider } from "../context/cart-context";
@@ -7,6 +8,15 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CartContextProvider>
+      <Head>
+        <title>Front-end Store | An e-commerce demo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta
+          name="description"
+          content="This is a template that I personally use to learn and as a reference. It is supposed to be a complete e-commerce website, with all the integrations that something like that requires."
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
