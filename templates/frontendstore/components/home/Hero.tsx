1c55fe61f53ffac9e6f8f5af1abba3976456dc02
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-05-23 10:46:29 +0200

    Add links to the hero content

--- templates/frontendstore/components/home/Hero.tsx
+++ templates/frontendstore/components/home/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
@@ -9,47 +10,59 @@ export default function Hero() {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-12 justify-items-center grayscale-[50%] group-hover:filter-none transition-all duration-600 ease-in-out">
         <div className="p-12 bg-sky-900 w-full flex justify-center col-span-8">
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.5, ease: "linear" }}
-          >
-            <Image
-              alt="React"
-              src="/techs/ReactLogo.svg"
-              width={1100}
-              height={1068}
-              className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out scale-95"
-            />
-          </motion.div>
+          <Link href="/products/react">
+            <a>
+              <motion.div
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.5, ease: "linear" }}
+              >
+                <Image
+                  alt="React"
+                  src="/techs/ReactLogo.svg"
+                  width={1100}
+                  height={1068}
+                  className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out scale-95"
+                />
+              </motion.div>
+            </a>
+          </Link>
         </div>
         <div className="col-span-4 w-full h-full flex flex-col justify-around">
           <div className="p-12 bg-black flex justify-center items-center h-full">
-            <motion.div
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3, ease: "linear" }}
-            >
-              <Image
-                alt="Next JS"
-                src="/techs/next-black.svg"
-                width={414}
-                height={248}
-                className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out"
-              />
-            </motion.div>
+            <Link href="/products/next">
+              <a>
+                <motion.div
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ duration: 0.3, ease: "linear" }}
+                >
+                  <Image
+                    alt="Next JS"
+                    src="/techs/next-black.svg"
+                    width={414}
+                    height={248}
+                    className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out"
+                  />
+                </motion.div>
+              </a>
+            </Link>
           </div>
           <div className="p-12 bg-violet-300 flex justify-center items-center h-full">
-            <motion.div
-              whileHover={{ scale: 1.05 }}
-              transition={{ duration: 0.3, ease: "linear" }}
-            >
-              <Image
-                alt="Redux"
-                src="/techs/reduxlogo.svg"
-                width={400}
-                height={400}
-                className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out"
-              />
-            </motion.div>
+            <Link href="/products/redux">
+              <a>
+                <motion.div
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ duration: 0.3, ease: "linear" }}
+                >
+                  <Image
+                    alt="Redux"
+                    src="/techs/reduxlogo.svg"
+                    width={400}
+                    height={400}
+                    className="blur-sm group-hover:filter-none transition-all duration-700 ease-in-out"
+                  />
+                </motion.div>
+              </a>
+            </Link>
           </div>
         </div>
       </div>
