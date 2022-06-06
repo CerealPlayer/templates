4268c25dc2417699a1699e366c4f957d360524bc
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-06-06 14:11:09 +0200

    Add on auth button to cart page

--- templates/frontendstore/components/cart/GoToCheckout.tsx
+++ templates/frontendstore/components/cart/GoToCheckout.tsx
@@ -1,7 +1,20 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import ActionButton from "../UI/ActionButton";
 
 export default function GoToCheckout() {
+  const { status } = useSession();
+  if (status === "authenticated") {
+    return (
+      <div className="lg:col-span-6 border border-slate-300 rounded-xl px-4 py-8 max-h-28 flex flex-col gap-4 items-center">
+        <Link href="/checkout">
+          <a className="max-w-xs w-full block">
+            <ActionButton primary>Go to checkout</ActionButton>
+          </a>
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className="lg:col-span-6 border border-slate-300 rounded-xl px-4 py-8 max-h-52 flex flex-col gap-4 items-center">
       <Link href="/login?ref=cart">
@@ -11,7 +24,9 @@ export default function GoToCheckout() {
       </Link>
       <span>or</span>
       <Link href="/checkout">
-        <a className="border-b border-slate-300 hover:border-slate-700 transition-all duration-200">Continue as a guest</a>
+        <a className="border-b border-slate-300 hover:border-slate-700 transition-all duration-200">
+          Continue as a guest
+        </a>
       </Link>
     </div>
   );
