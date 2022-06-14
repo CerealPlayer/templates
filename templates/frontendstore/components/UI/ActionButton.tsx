da462370572d4a0175de504e6d316367a651fb5c
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-06-14 09:08:42 +0200

    Adjust button colors

--- templates/frontendstore/components/UI/ActionButton.tsx
+++ templates/frontendstore/components/UI/ActionButton.tsx
@@ -15,8 +15,8 @@ export default function ActionButton({
   onClick,
 }: props) {
   const style = primary
-    ? "px-8 py-4 w-full rounded-lg hover:bg-[#7B6D59] transition-all duration-100 ease-out text-slate-50 font-bold bg-slate-800"
-    : "px-8 w-full py-4 rounded-lg text-slate-50 font-bold bg-gray-500 hover:bg-[#AFA28F] transition-all duration-100 ease-out";
+    ? "px-8 py-4 w-full rounded-lg hover:bg-[#4F4028] transition-all duration-100 ease-out text-slate-50 font-bold bg-slate-800"
+    : "px-8 w-full py-4 rounded-lg text-slate-50 font-bold bg-gray-500 hover:bg-[#90887A] transition-all duration-100 ease-out";
   return (
     <button type={type} className={style} onClick={onClick}>
       {children}
