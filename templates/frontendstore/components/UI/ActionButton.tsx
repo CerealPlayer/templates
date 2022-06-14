ad225a1f8e9b0194030f5a01564d31aca639ee08
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-06-14 09:36:07 +0200

    Make buttons monochromatic

--- templates/frontendstore/components/UI/ActionButton.tsx
+++ templates/frontendstore/components/UI/ActionButton.tsx
@@ -15,8 +15,8 @@ export default function ActionButton({
   onClick,
 }: props) {
   const style = primary
-    ? "px-8 py-4 w-full rounded-lg hover:bg-[#4F4028] transition-all duration-100 ease-out text-slate-50 font-bold bg-slate-800"
-    : "px-8 w-full py-4 rounded-lg text-slate-50 font-bold bg-gray-500 hover:bg-[#90887A] transition-all duration-100 ease-out";
+    ? "px-8 py-4 w-full rounded-lg hover:bg-[#2F405D] transition-all duration-100 ease-out text-slate-50 font-bold bg-slate-800"
+    : "px-8 w-full py-4 rounded-lg text-slate-50 font-bold bg-gray-500 hover:bg-[#858C99] transition-all duration-100 ease-out";
   return (
     <button type={type} className={style} onClick={onClick}>
       {children}
