b3a036186ab2bbeb05a0aaefcc084eabd05c968e
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-06-18 21:12:17 +0200

    Add more tests, add react testing library

--- templates/frontendstore/components/UI/__tests__/Button.test.js
+++ templates/frontendstore/components/UI/__tests__/Button.test.js
@@ -1,8 +1,43 @@
 import Button from "../Button";
+import ActionButton from "../ActionButton";
 import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
 
-test("Button is disabled", () => {
-  const component = renderer.create(<Button dis>Hey</Button>);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe("Test UI elements", () => {
+  test("Renders a common button", () => {
+    const tree = renderer.create(<Button>Click Me</Button>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test("Renders an action primary button", () => {
+    const clickHandler = jest.fn();
+    const tree = renderer
+      .create(
+        <ActionButton primary type="button" onClick={clickHandler}>
+          Click Me
+        </ActionButton>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test("Renders an action secondary button", () => {
+    const clickHandler = jest.fn();
+    const tree = renderer
+      .create(
+        <ActionButton type="button" onClick={clickHandler}>
+          Click Me
+        </ActionButton>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test("Click handler is callable in action button", () => {
+    const clickHandler = jest.fn();
+    const { getByText } = render(
+      <ActionButton primary type="button" onClick={clickHandler}>
+        Click Me
+      </ActionButton>
+    );
+    fireEvent.click(getByText(/click me/i));
+    expect(clickHandler).toHaveBeenCalledTimes(1);
+  });
 });
