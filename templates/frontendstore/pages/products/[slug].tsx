87cade612b69fd40c84cca08aab3f5ecee1a8215
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-07-05 19:44:25 +0200

    Add dynamic product page rendering

--- templates/frontendstore/pages/products/[slug].tsx
+++ templates/frontendstore/pages/products/[slug].tsx
@@ -1,11 +1,48 @@
+import { readFile } from "fs/promises";
+import { useRouter } from "next/router";
 import BoughtTogether from "../../components/product-detail/BoughtTogether";
 import ProductInfo from "../../components/product-detail/ProductInfo";
+import { retrieveData } from "../../helpers";
+import { products } from "../../types/props";
 
-export default function ProductDetail() {
+export default function ProductDetail({ products }: { products: products }) {
+  const { query } = useRouter();
+  const { slug } = query;
+  if (!products) {
+    return null;
+  }
+  const currentItem = products.find((product) => product.name.toLowerCase() === slug);
+  if (!currentItem) {
+    return (
+      <div className="flex justify-center items-center min-h-body">
+        <h1>No item with this name exists. Yet.</h1>
+      </div>
+    );
+  }
   return (
-    <main>
-      <ProductInfo />
+    <>
+      <ProductInfo item={currentItem} />
       <BoughtTogether />
-    </main>
+    </>
   );
+}
+
+export async function getStaticPaths() {
+  const products = await retrieveData();
+  const allNames = products.map((product) => product.name);
+  const paths = allNames.map((name) => ({ params: { slug: name } }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps() {
+  const products = await retrieveData();
+  return {
+    props: {
+      products: products,
+    },
+  };
 }
--- templates/frontendstore/types/props.ts
+++ templates/frontendstore/types/props.ts
@@ -4,6 +4,7 @@ export type products = {
   short_desc: string;
   src: string;
   href: string;
+  price: number;
 }[];
 
 export type ArrayElement<ArrayType extends readonly unknown[]> =
