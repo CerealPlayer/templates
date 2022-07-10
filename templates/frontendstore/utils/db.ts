20ec777cfdef7d05d317bd7740ac67879974398a
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-07-10 20:11:18 +0200

    Correctly query products

--- templates/frontendstore/utils/db.ts
+++ templates/frontendstore/utils/db.ts
@@ -1,6 +1,6 @@
-import { Client } from "pg";
+import { Pool } from "pg";
 const connectionString = process.env.POSTGRES_CONNECTION_URI;
 
-export const client = new Client({
+export const pool = new Pool({
   connectionString
 });
