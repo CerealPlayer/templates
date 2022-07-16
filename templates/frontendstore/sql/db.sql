0f26636aa28a2d7e1d127a501702339c8a56b08e
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-07-16 09:16:10 +0200

    Populate db on mount

--- templates/frontendstore/sql/db.sql
+++ templates/frontendstore/sql/db.sql
@@ -7,3 +7,11 @@ CREATE TABLE products (
   price DECIMAL NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO products (name, short_desc, src, href, price) VALUES 
+('React', 'Everybody loves (hates) it', '/techs/ReactLogo.svg', '/products/react', 18.1),
+('Angular', 'What else do you need', '/techs/angularlogo.svg', '/products/angular', 14),
+('Vue', 'The cool kid', '/techs/vuelogo.svg', '/products/vue', 26.14),
+('Svelte', 'The next rockstar', '/techs/sveltelogo.svg', '/products/svelte', 34.8),
+('Lit', 'You should check it out', '/techs/litlogo.svg', '/products/lit', 22.5),
+('Solid', 'Doing things right', '/techs/solidlogo.svg', '/products/solid', 14.3);
